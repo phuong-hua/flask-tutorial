de202d4c6b64b9be6c8741a8e5900b3ca985e112
DESKTOP-C0HL1O1\npqhu <mphuonghua@gmail.com>
2022-02-19 21:09:13 +0800

    day 1

---
+++ hello.py
@@ -0,0 +1,18 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+   return 'Hello World'
+   
+@app.route('/hello')   
+def hello_world():
+   return 'hello world'
+
+@app.route('/hello/<name>')
+def hello_name(name):
+   return 'Hello %s!' % name
+
+if __name__ == '__main__':
+   app.run(debug = True)
+   
